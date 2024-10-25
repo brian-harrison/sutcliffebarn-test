329370106927cd1548fdefc36a8836c4b5eb3986
Brian Harrison <brian@thewebofinteriors.com>
2024-10-25 08:58:38 +0100

    Playing with changes

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const Home = function () {
   return (
     <>
       <p>This is the home page for the test site</p>
-      <p><i>We will keep adding to this page............</i></p>
+      <p><i>We will keep adding to this page...</i></p>
     </>
   );
 };
