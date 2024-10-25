f7518523753556772d78e27376e6315f3e5cb3a9
Brian Harrison <brian@thewebofinteriors.com>
2024-10-25 08:53:53 +0100

    Added basic files

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+const Home = function () {
+  return (
+    <>
+      <p>This is the home page for the test site</p>
+      <p><i>We will keep adding to this page............</i></p>
+    </>
+  );
+};
+
+export default Home;
